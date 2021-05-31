483152553773e6e695c25d988afdc91fc57331de
Wayan Sudiarta <wayan@tix.com.au>
2021-05-31 12:47:25 +0800

    Add more items into playgroud pages

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -11,66 +11,198 @@ const IndexPage = () => (
       <div id="projects" className="prose">
         <h2 className="section-headline">Playground</h2>
         <div className="mb-16">
-          <h3 id="chrisko-headline">Fast Api Projects</h3>
+          <h3 id="chrisko-headline">Fast Api</h3>
           <section className="mb-6">
               Build custom api using Fast Api Framework
           </section>
-          <section className="mb-6">
-            <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
-              Deployed to:
-            </div>
-            <div className="lg:inline-block lg:w-8/12 w-full">
-              <a href="https://wysdi-fastapi.vercel.app" target="_blank" rel="noopener noreferrer">wysdi-fastapi.vercel.app</a>
-            </div>
-          </section>
           {/*<section className="mb-6">*/}
           {/*  <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">*/}
-          {/*    Code on:*/}
+          {/*    Deployed to:*/}
           {/*  </div>*/}
           {/*  <div className="lg:inline-block lg:w-8/12 w-full">*/}
-          {/*    <a href="https://github.com/christiankozalla/nextjs-blog" target="_blank"*/}
-          {/*       rel="noopener noreferrer">GitHub/nextjs-blog</a>*/}
+          {/*    <a href="https://wysdi-fastapi.vercel.app" target="_blank" rel="noopener noreferrer">wysdi-fastapi.vercel.app</a>*/}
           {/*  </div>*/}
           {/*</section>*/}
+          <section className="mb-6">
+            <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
+              Code on:
+            </div>
+            <div className="lg:inline-block lg:w-8/12 w-full">
+              <a href="https://github.com/wysdi/fastapi-vercel" target="_blank"
+                 rel="noopener noreferrer">Wysdi/fastapi-vercel</a>
+            </div>
+          </section>
           <section className="mb-6">
             <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
               Built with:
             </div>
             <div className="lg:inline-block lg:w-8/12 w-full">
-              <span className="rounded-xl bg-blue-300 px-3">Python</span>
+              <span className="rounded-xl bg-blue-300 px-3 mr-4">Python</span>
               <span className="rounded-xl bg-blue-300 px-3">Fast Api</span>
             </div>
           </section>
         </div>
         <div className="mb-16">
-          <h3 id="chrisko-headline">Gowes Site</h3>
+          <h3 id="chrisko-headline">Telegram Bot</h3>
           <section className="mb-6">
-            Fetch video content from youtube and show it on specific sites
-          </section>
-          <section className="mb-6">
-            <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
-              Deployed to:
-            </div>
-            <div className="lg:inline-block lg:w-8/12 w-full">
-              <a href="https://wysdi.github.io/gowes" target="_blank" rel="noopener noreferrer">wysdi.github.io/gowes</a>
-            </div>
+            Bots are simply Telegram accounts operated by software – not people – and they'll often have AI features. They can do anything – teach, play, search, broadcast, remind, connect, integrate with other services, or even pass commands to the Internet of Things
           </section>
           {/*<section className="mb-6">*/}
           {/*  <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">*/}
-          {/*    Code on:*/}
+          {/*    Deployed to:*/}
           {/*  </div>*/}
           {/*  <div className="lg:inline-block lg:w-8/12 w-full">*/}
-          {/*    <a href="https://github.com/christiankozalla/nextjs-blog" target="_blank"*/}
-          {/*       rel="noopener noreferrer">GitHub/nextjs-blog</a>*/}
+          {/*    <a href="https://wysdi.github.io/gowes" target="_blank" rel="noopener noreferrer">wysdi.github.io/gowes</a>*/}
           {/*  </div>*/}
           {/*</section>*/}
+          <section className="mb-6">
+            <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
+              Code on:
+            </div>
+            <div className="lg:inline-block lg:w-8/12 w-full">
+              <a href="https://github.com/wysdi/wysdi-tbot" target="_blank"
+                 rel="noopener noreferrer">GitHub/wysdi-tbot</a>
+            </div>
+          </section>
           <section className="mb-6">
             <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
               Built with:
             </div>
             <div className="lg:inline-block lg:w-8/12 w-full">
+              <span className="rounded-xl bg-blue-300 px-3 mr-4">Python</span>
+              <span className="rounded-xl bg-blue-300 px-3">Telegram</span>
+            </div>
+          </section>
+        </div>
+        <div className="mb-16">
+          <h3 id="chrisko-headline">Salesforce Custom Block</h3>
+          <section className="mb-6">
+            Sample Salesforce Marketing Cloud Content Builder Block using the blocksdk.
+          </section>
+          {/*<section className="mb-6">*/}
+          {/*  <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">*/}
+          {/*    Deployed to:*/}
+          {/*  </div>*/}
+          {/*  <div className="lg:inline-block lg:w-8/12 w-full">*/}
+          {/*    <a href="https://wysdi.github.io/gowes" target="_blank" rel="noopener noreferrer">wysdi.github.io/gowes</a>*/}
+          {/*  </div>*/}
+          {/*</section>*/}
+          <section className="mb-6">
+            <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
+              Code on:
+            </div>
+            <div className="lg:inline-block lg:w-8/12 w-full">
+              <a href="https://github.com/wysdi/salesforce-custom-block" target="_blank"
+                 rel="noopener noreferrer">Wysdi/salesforce-custom-block</a>
+            </div>
+          </section>
+          <section className="mb-6">
+            <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
+              Built with:
+            </div>
+            <div className="lg:inline-block lg:w-8/12 w-full">
+              <span className="rounded-xl bg-blue-300 px-3 mr-4">Alpine Js</span>
+              <span className="rounded-xl bg-blue-300 px-3 mr-4">Handlebars</span>
+              <span className="rounded-xl bg-blue-300 px-3 mr-4">Express</span><br/>
+              <span className="rounded-xl bg-blue-300 px-3 mr-4">Lightning Design System</span>
+            </div>
+          </section>
+        </div>
+        <div className="mb-16">
+          <h3 id="chrisko-headline">CSV Extractor</h3>
+          <section className="mb-6">
+            This pipeline will extract the csv lines from text files
+          </section>
+          {/*<section className="mb-6">*/}
+          {/*  <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">*/}
+          {/*    Deployed to:*/}
+          {/*  </div>*/}
+          {/*  <div className="lg:inline-block lg:w-8/12 w-full">*/}
+          {/*    <a href="https://wysdi.github.io/gowes" target="_blank" rel="noopener noreferrer">wysdi.github.io/gowes</a>*/}
+          {/*  </div>*/}
+          {/*</section>*/}
+          <section className="mb-6">
+            <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
+              Code on:
+            </div>
+            <div className="lg:inline-block lg:w-8/12 w-full">
+              <a href="https://github.com/wysdi/python-beam" target="_blank"
+                 rel="noopener noreferrer">Wysdi/python-beam</a>
+            </div>
+          </section>
+          <section className="mb-6">
+            <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
+              Built with:
+            </div>
+            <div className="lg:inline-block lg:w-8/12 w-full">
+              <span className="rounded-xl bg-blue-300 px-3 mr-4">Apache Beam</span>
               <span className="rounded-xl bg-blue-300 px-3">Python</span>
-              <span className="rounded-xl bg-blue-300 px-3">Fast Api</span>
+            </div>
+          </section>
+        </div>
+        <div className="mb-16">
+          <h3 id="chrisko-headline">Course Scrapper</h3>
+          <section className="mb-6">
+            Scrapping the course item from buildwithangga, codepolitan and dicoding using scrapy
+          </section>
+          {/*<section className="mb-6">*/}
+          {/*  <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">*/}
+          {/*    Deployed to:*/}
+          {/*  </div>*/}
+          {/*  <div className="lg:inline-block lg:w-8/12 w-full">*/}
+          {/*    <a href="https://wysdi.github.io/gowes" target="_blank" rel="noopener noreferrer">wysdi.github.io/gowes</a>*/}
+          {/*  </div>*/}
+          {/*</section>*/}
+          <section className="mb-6">
+            <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
+              Code on:
+            </div>
+            <div className="lg:inline-block lg:w-8/12 w-full">
+              <a href="https://github.com/wysdi/scrapy" target="_blank"
+                 rel="noopener noreferrer">Wysdi/scrapy</a>
+            </div>
+          </section>
+          <section className="mb-6">
+            <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
+              Built with:
+            </div>
+            <div className="lg:inline-block lg:w-8/12 w-full">
+              <span className="rounded-xl bg-blue-300 px-3 mr-4">Python</span>
+              <span className="rounded-xl bg-blue-300 px-3 mr-4">Scrapy</span>
+              <span className="rounded-xl bg-blue-300 px-3 mr-4">Selenium</span>
+              <span className="rounded-xl bg-blue-300 px-3">WebDriver</span>
+            </div>
+          </section>
+        </div>
+        <div className="mb-16">
+          <h3 id="chrisko-headline">Gowes (Personal Sites)</h3>
+          <section className="mb-6">
+            Fetch video content from youtube channel and show it on React App
+          </section>
+          {/*<section className="mb-6">*/}
+          {/*  <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">*/}
+          {/*    Deployed to:*/}
+          {/*  </div>*/}
+          {/*  <div className="lg:inline-block lg:w-8/12 w-full">*/}
+          {/*    <a href="https://wysdi.github.io/gowes" target="_blank" rel="noopener noreferrer">wysdi.github.io/gowes</a>*/}
+          {/*  </div>*/}
+          {/*</section>*/}
+          <section className="mb-6">
+            <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
+              Code on:
+            </div>
+            <div className="lg:inline-block lg:w-8/12 w-full">
+              <a href="https://github.com/wysdi/gowes" target="_blank"
+                 rel="noopener noreferrer">Wysdi/gowes</a>
+            </div>
+          </section>
+          <section className="mb-6">
+            <div className="lg:inline-block lg:w-3/12 lg:align-top italic mb-2">
+              Built with:
+            </div>
+            <div className="lg:inline-block lg:w-8/12 w-full">
+              <span className="rounded-xl bg-blue-300 px-3 mr-4">React</span>
+              <span className="rounded-xl bg-blue-300 px-3">YouTube Api</span>
             </div>
           </section>
         </div>
